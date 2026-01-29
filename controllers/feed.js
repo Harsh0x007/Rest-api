7bf0b12ec0c232151ed80479e95d225cb1d226be
Harsh0x007 <chavdahj123@gmail.com>
2026-01-29 20:36:01 +0530

    add a little error handling

--- controllers/feed.js
+++ controllers/feed.js
@@ -19,10 +19,9 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
-        return res.status(422).json({
-            message: 'validation failed, entered data is incorrect.',
-            errors: errors.array()
-        })
+        const error = new Error('Validation failed, entered data is incorrect.')
+        error.statuscode = 422;
+        throw error;
     }
     const title = req.body.title
     const content = req.body.content
@@ -39,6 +38,9 @@ exports.createPost = (req, res, next) => {
             post: result
         })
     }).catch(err => {
-        console.log(err)
+        if (!err.statusCode) {
+            err.statusCode = 500
+        }
+        next(err);
     })
 }
