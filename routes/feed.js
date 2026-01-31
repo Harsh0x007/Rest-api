c7aa1fed03b618f5b807c444bc0765ac9e723bf0
Harsh0x007 <chavdahj123@gmail.com>
2026-02-01 01:23:08 +0530

    update the post

--- routes/feed.js
+++ routes/feed.js
@@ -19,4 +19,14 @@ router.post('/post', [
 )
 
 router.get('/post/:postId', feedController.getPost);
+
+router.put('/post/:postId', [
+    body('title')
+        .trim()
+        .isLength({ min: 7 }),
+    body('content')
+        .trim()
+        .isLength({ min: 5 })
+], feedController.updatePost)
+
 module.exports = router;
